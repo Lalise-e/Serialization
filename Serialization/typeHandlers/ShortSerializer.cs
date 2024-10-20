6d756d6ee55b629b1264a65f3574f1426f3717ec
Alice <sten123465@gmail.com>
2024-10-20 05:53:01 +0200

    Implemented ShortSerializer

---
+++ Serialization/typeHandlers/ShortSerializer.cs
@@ -0,0 +1,17 @@
+﻿namespace Serialization.typeHandlers
+{
+	internal class ShortSerializer : ISerialization
+	{
+		public Type SerializationType { get { return typeof(short); } }
+
+		public object? Deserialize(byte[] serializedBytes)
+		{
+			return BitConverter.ToInt16(serializedBytes);
+		}
+
+		public byte[] Serialize(object? obj)
+		{
+			return BitConverter.GetBytes((short)obj);
+		}
+	}
+}
