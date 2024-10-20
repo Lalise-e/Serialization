1b536438540beace49ee0308b6be0e2c421e32a9
Alice <sten123465@gmail.com>
2024-10-20 06:03:25 +0200

    Implemented ULongSerializer

---
+++ Serialization/typeHandlers/ULongSerializer.cs
@@ -0,0 +1,17 @@
+﻿namespace Serialization.typeHandlers
+{
+	internal class ULongSerializer : ISerialization
+	{
+		public Type SerializationType { get { return typeof(ulong); } }
+
+		public object? Deserialize(byte[] serializedBytes)
+		{
+			return BitConverter.ToUInt64(serializedBytes);
+		}
+
+		public byte[] Serialize(object? obj)
+		{
+			return BitConverter.GetBytes((ulong)obj);
+		}
+	}
+}
