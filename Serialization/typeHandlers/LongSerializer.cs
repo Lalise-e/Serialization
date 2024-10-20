a92f9c7945bfb28c66654ca154d36cb666c852f7
Alice <sten123465@gmail.com>
2024-10-20 05:56:30 +0200

    Implemented LongSerializer

---
+++ Serialization/typeHandlers/LongSerializer.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Serialization.typeHandlers
+{
+	internal class LongSerializer : ISerialization
+	{
+		public Type SerializationType { get { return typeof(long); } }
+
+		public object? Deserialize(byte[] serializedBytes)
+		{
+			return BitConverter.ToInt64(serializedBytes);
+		}
+
+		public byte[] Serialize(object? obj)
+		{
+			return BitConverter.GetBytes((long)obj);
+		}
+	}
+}
