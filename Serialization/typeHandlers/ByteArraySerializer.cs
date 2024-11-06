0b4eef20276218b7eb43fa73b58abab28bfb4a95
Alice <sten123465@gmail.com>
2024-11-06 05:59:25 +0100

    Added ByteArraySerializer

---
+++ Serialization/typeHandlers/ByteArraySerializer.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Serialization.typeHandlers
+{
+	internal class ByteArraySerializer : ISerialization
+	{
+		public Type SerializationType { get { return typeof(byte[]); } }
+
+		public bool IsStaticLength { get { return false; } }
+
+		public int Length { get { return 0; } }
+
+		public object? Deserialize(byte[] serializedBytes)
+		{
+			return serializedBytes;
+		}
+
+		public byte[] Serialize(object? obj)
+		{
+			return (byte[])obj;
+		}
+	}
+}
